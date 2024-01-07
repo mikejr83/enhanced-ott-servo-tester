e91763007c97919b0abea54c2ea3ff8e61d6bd37
Michael Gardner <mikejr83@gmail.com>
2024-01-07 11:00:29 -0500

    Things work

---
+++ include/TextPrinter.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+
+void pintTitleScreen(Adafruit_SSD1351 oled);
--- include/settings.h
+++ include/settings.h
@@ -1,9 +1,26 @@
 #pragma once
 
-/****************************************************************************************
-    //Use the  library function definition
-*****************************************************************************************/
-//GPIO config
-#define OLED_CS  22
-#define OLED_RST 21
-#define OLED_DC  20
+// OLED pins
+#define SCLK_PIN 18
+#define MOSI_PIN 19
+#define DC_PIN 20
+#define CS_PIN 22
+#define RST_PIN 21
+
+// Screen dimensions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 128 
+
+// Key inputs pins
+#define BUTTON_ONE 21
+#define BUTTON_TWO 20
+
+// Color definitions
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
