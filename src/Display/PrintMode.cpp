dbae95770939ff93fd08e95066b45199c68bf3eb
Michael Gardner <mikejr83@gmail.com>
2024-01-24 08:42:21 -0500

    Print green servo values.

--- src/Display/PrintMode.cpp
+++ src/Display/PrintMode.cpp
@@ -2,7 +2,6 @@
 
 // typedef drawCenteredLineTextString =
 
-
 void Display::PrintMode(RunMode runMode)
 {
     // print the title first
@@ -46,16 +45,21 @@ void Display::printServoPositions()
     int rows = ceil(totalEnabled / perRow);
     int baseOffset = (128 - rows * 9) - 3;
 
-    // oled.drawRect(0, baseOffset - 2, SCREEN_WIDTH, 128 - baseOffset, BLACK);
+    oled.setTextSize(1);
+
     for (int i = 0; i < rows; i++)
     {
+        String servoName[(int)perRow];
+        String currentValue[(int)perRow];
         String lineOfText = "";
-        for (int j = i * perRow; j < (i * perRow + perRow) && j < totalEnabled; j++)
+        for (int j = i * perRow, k = 0; j < (i * perRow + perRow) && j < totalEnabled; j++, k++)
         {
-            lineOfText.append((char)(65 + j));
-            lineOfText.append(": ");
-            lineOfText.append(enabledServos[j].curV);
-            lineOfText.append("ms ");
+            servoName[k] = String((char)(65 + j));
+            servoName[k].append(": ");
+            lineOfText.append(servoName[k]);
+            currentValue[k] = String(enabledServos[j].curV);
+            lineOfText.append(currentValue[k]);
+            lineOfText.append(F("ms "));
         }
 
         PRINT("Line: ", lineOfText);
@@ -63,10 +67,24 @@ void Display::printServoPositions()
         char buf[lineOfText.length()];
         lineOfText.toCharArray(buf, lineOfText.length());
 
-        oled.setTextColor(WHITE, BLACK);
-        oled.setTextSize(1);
-
         int y = baseOffset + i * 9;
-        Display::DrawCenteredLineTextString(oled, buf, 0, y);
+
+        int16_t x1, y1;
+        uint16_t w, h;
+        oled.getTextBounds(buf, 0, y, &x1, &y1, &w, &h);
+
+        oled.setCursor((SCREEN_WIDTH - w) / 2, y);
+
+        for (int i = 0; i < perRow; i++)
+        {
+            oled.setTextColor(WHITE, BLACK);
+            oled.print(servoName[i]);
+
+            oled.setTextColor(GREEN, BLACK);
+            oled.print(currentValue[i]);
+
+            oled.setTextColor(GREEN_DARK, BLACK);
+            oled.print(F("ms "));
+        }
     }
 }
